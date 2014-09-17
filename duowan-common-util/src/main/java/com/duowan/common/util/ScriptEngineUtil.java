8f476668c0416e38dd0e0a1a24d7fc0e28d395ba
badqiu <badqiu@gmail.com>
2014-09-17 13:12:24 +0800

    add Graph
    Signed-off-by: badqiu <badqiu@gmail.com>

--- duowan-common-util/src/main/java/com/duowan/common/util/ScriptEngineUtil.java
+++ duowan-common-util/src/main/java/com/duowan/common/util/ScriptEngineUtil.java
@@ -14,6 +14,8 @@ import javax.script.ScriptEngineFactory;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
+import org.apache.commons.lang.StringUtils;
+
 public class ScriptEngineUtil {
 	public static ScriptEngineManager factory = new ScriptEngineManager();
 	
@@ -25,6 +27,9 @@ public class ScriptEngineUtil {
 	}
 	
 	public static Object eval(String scriptEngineName,String script,Map<? extends String, ? extends Object> context) {
+		if(StringUtils.isBlank(script)) {
+			return null;
+		}
         ScriptEngine engine = getScriptEngine(scriptEngineName);
         Bindings bindings = engine.createBindings();
         bindings.putAll(context);
@@ -64,104 +69,5 @@ public class ScriptEngineUtil {
 		return engine;
 	}
 	
-	private class ScriptEngineWrapper implements ScriptEngine,Compilable,Invocable  {
-		private ScriptEngine engine;
-		private Compilable compilable;
-		private Invocable invocable;
-		
-		public ScriptEngineWrapper(ScriptEngine engine) {
-			this.engine = engine;
-			if(engine instanceof Compilable) {
-				compilable = (Compilable)engine;
-			}
-			if(engine instanceof Invocable) {
-				invocable = (Invocable)engine;
-			}
-		}
-
-		public Object eval(String script, ScriptContext context)
-				throws ScriptException {
-			return engine.eval(script, context);
-		}
-
-		public Object eval(Reader reader, ScriptContext context)
-				throws ScriptException {
-			return engine.eval(reader, context);
-		}
-
-		public Object eval(String script) throws ScriptException {
-			return engine.eval(script);
-		}
-
-		public Object eval(Reader reader) throws ScriptException {
-			return engine.eval(reader);
-		}
-
-		public Object eval(String script, Bindings n) throws ScriptException {
-			return engine.eval(script, n);
-		}
-
-		public Object eval(Reader reader, Bindings n) throws ScriptException {
-			return engine.eval(reader, n);
-		}
-
-		public void put(String key, Object value) {
-			engine.put(key, value);
-		}
-
-		public Object get(String key) {
-			return engine.get(key);
-		}
-
-		public Bindings getBindings(int scope) {
-			return engine.getBindings(scope);
-		}
-
-		public void setBindings(Bindings bindings, int scope) {
-			engine.setBindings(bindings, scope);
-		}
-
-		public Bindings createBindings() {
-			return engine.createBindings();
-		}
-
-		public ScriptContext getContext() {
-			return engine.getContext();
-		}
-
-		public void setContext(ScriptContext context) {
-			engine.setContext(context);
-		}
-
-		public ScriptEngineFactory getFactory() {
-			return engine.getFactory();
-		}
-
-		public CompiledScript compile(String script) throws ScriptException {
-			return compilable.compile(script);
-		}
-
-		public CompiledScript compile(Reader script) throws ScriptException {
-			return compilable.compile(script);
-		}
-
-		public Object invokeMethod(Object thiz, String name, Object... args)
-				throws ScriptException, NoSuchMethodException {
-			return invocable.invokeMethod(thiz, name, args);
-		}
-
-		public Object invokeFunction(String name, Object... args)
-				throws ScriptException, NoSuchMethodException {
-			return invocable.invokeFunction(name, args);
-		}
-
-		public <T> T getInterface(Class<T> clasz) {
-			return invocable.getInterface(clasz);
-		}
-
-		public <T> T getInterface(Object thiz, Class<T> clasz) {
-			return invocable.getInterface(thiz, clasz);
-		}
-	}
 	
 }
---
+++ duowan-common-util/src/test/java/com/duowan/common/retry/RetryInterceptor.java
@@ -0,0 +1,12 @@
+package com.duowan.common.retry;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+public class RetryInterceptor implements MethodInterceptor{
+
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		return null;
+	}
+
+}
