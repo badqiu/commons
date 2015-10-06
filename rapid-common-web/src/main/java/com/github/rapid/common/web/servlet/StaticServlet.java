ac417eff6a1163e9e53084585af53d75de02632f
badqiu <badqiu@gmail.com>
2015-10-06 11:32:54 +0800

    1

---
+++ rapid-common-web/src/main/java/com/github/rapid/common/web/servlet/StaticServlet.java
@@ -0,0 +1,71 @@
+package com.github.rapid.common.web.servlet;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.util.Assert;
+
+/**
+ * 处理静态资源的serlvet
+ * @author badqiu
+ *
+ */
+public class StaticServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String localPath = null;
+	
+	private ServletContext servletContext;
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		localPath = config.getInitParameter("localPath");
+		Assert.hasLength(localPath, "'localPath' must be not blank");
+		this.servletContext = config.getServletContext();
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String path = req.getRequestURI();
+		setResponseHeaders(resp, path);
+		writeFile2Response(resp, path);
+	}
+
+	private void writeFile2Response(HttpServletResponse resp, String path)
+			throws FileNotFoundException, IOException {
+		File inputFile = new File(localPath,path);
+		InputStream input = new FileInputStream(inputFile);
+		OutputStream output = resp.getOutputStream();
+		try {
+			IOUtils.copy(input, output);
+		}finally {
+			IOUtils.closeQuietly(input);
+			IOUtils.closeQuietly(output);
+		}
+	}
+
+	private void setResponseHeaders(HttpServletResponse resp, String path) {
+		String mimeType = servletContext.getMimeType(new File(path).getName());
+		if(mimeType == null) {
+			resp.setHeader("Content-Disposition", "attachment; filename=\"" + new File(path).getName() + "\"");
+		}else 
+			resp.setHeader("Content-Type", mimeType);
+			if(!mimeType.contains("text")) {
+				resp.setHeader("Content-Disposition", "attachment; filename=\"" + new File(path).getName() + "\"");
+		}
+	}
+}
