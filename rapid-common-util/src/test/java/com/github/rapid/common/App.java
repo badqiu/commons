68c4a568fdfe3cc026129f29cf0363220cb7e9f9
badqiu <badqiu@gmail.com>
2019-06-11 19:11:57 +0800

    1

--- rapid-common-util/src/test/java/com/github/rapid/common/App.java
+++ rapid-common-util/src/test/java/com/github/rapid/common/App.java
@@ -4,13 +4,17 @@ public class App {
 
 	private String app;
 	private String baseDir = "/data";
+	
 	private String getDataDir() {
 		return "/data/"+app+"/data";
 	}
+	
 	private String getConfigDir() {
 		return "/data/"+app+"/conf";
 	}
+	
 	private String getLogDir() {
 		return "/data/log/"+app;
 	}
+	
 }
