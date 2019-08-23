8677bdb3117c05f8b21d74a299b585b38428bcc5
badqiu <badqiu@gmail.com>
2019-08-23 10:01:22 +0800

    1

---
+++ rapid-common-rest/src/test/java/com/github/rapid/common/rpc/server/RPCContextTest.java
@@ -0,0 +1,29 @@
+package com.github.rapid.common.rpc.server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import com.github.rapid.common.util.Profiler;
+
+public class RPCContextTest {
+
+	@Test
+	public void test() {
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		RPCContext.setRequest(request);
+		
+		long count = 10000000000L;
+		Profiler.start("RPCContext.getRequest()",count);
+		for(long i = 0; i < count; i++) {
+			RPCContext.getRequest();
+		}
+		Profiler.release();
+		
+		
+		System.out.println(Profiler.dump());
+		RPCContext.clear();
+	}
+
+}
