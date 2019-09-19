7e798e511c9cb2ac8b75d9d29965d088a7029145
badqiu <badqiu@gmail.com>
2019-09-19 10:47:26 +0800

    1

---
+++ rapid-common-util/src/test/java/com/github/rapid/common/perf_test/ReflectPerfTest.java
@@ -0,0 +1,27 @@
+package com.github.rapid.common.perf_test;
+
+import java.lang.reflect.Method;
+
+import org.junit.Test;
+
+import com.github.rapid.common.util.Profiler;
+import com.github.rapid.common.util.graph.Graph;
+
+public class ReflectPerfTest {
+	
+	@Test
+	public void testMethod() throws Exception {
+		Graph g = new Graph();
+		Method m = g.getClass().getMethod("getNodes");
+		m.invoke(g);
+		
+		int count = 1000000000;
+		Profiler.start(count);
+		for(int i = 0; i < count; i++)
+			m.invoke(g);
+		Profiler.release();
+		
+		System.out.println(Profiler.dump());
+	}
+	
+}
