3eea0ae6f89caed86238dcdea5b7a8a22b8b19f4
badqiu <badqiu@gmail.com>
2020-07-21 10:03:32 +0800

    delete

--- rapid-common-rest/src/test/java/com/github/rapid/common/rpc/client/SimpleHttpInvokerRequestExecutorTest.java
+++ rapid-common-rest/src/test/java/com/github/rapid/common/rpc/client/SimpleHttpInvokerRequestExecutorTest.java
@@ -11,7 +11,7 @@ public class SimpleHttpInvokerRequestExecutorTest {
 		 SimpleHttpInvokerRequestExecutor sh = new  SimpleHttpInvokerRequestExecutor();
 		 RPCRequest r = new RPCRequest();
 		 r.setArguments(new Object[]{123,456});
-		 sh.executeRequest("http://localhost:8080/userdata/importData.do?username=yuminxiong&password=iXZfu38dfdo20M8Odw9eJR5dfeQ2ogy4o0Oet4G&importMode=append&appId=lj&module=master", r);
+		 sh.executeRequest("http://localhost:8080/userdata/importData.do?username=test&password=iXZfu38dfdo20M8Odw9eJR5dfeQ2ogy4o0Oet4G&importMode=append&appId=lj&module=master", r);
 	}
 
 }
