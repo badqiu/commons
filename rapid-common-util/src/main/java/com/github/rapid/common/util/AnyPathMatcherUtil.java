1814e22c8a8bc1cdac0b83582a0c74e679f028a0
badqiu <badqiu@gmail.com>
2020-09-18 14:15:28 +0800

    1

---
+++ rapid-common-util/src/main/java/com/github/rapid/common/util/AnyPathMatcherUtil.java
@@ -0,0 +1,38 @@
+package com.github.rapid.common.util;
+
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.util.AntPathMatcher;
+
+public class AnyPathMatcherUtil {
+
+	static AntPathMatcher antPathMatcher = new AntPathMatcher();
+	
+	public static boolean match(String path, List<String> includePatterns,List<String> excludePatterns) {
+		if(CollectionUtils.isEmpty(includePatterns)) return false;
+		
+		if(AnyPathMatcherUtil.match(path, excludePatterns)) {
+			return false;
+		}
+		
+		if(AnyPathMatcherUtil.match(path, includePatterns)) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static boolean match(String path, List<String> patternList) {
+		if(CollectionUtils.isEmpty(patternList)) {
+			return false;
+		}
+		
+		for(String pattern : patternList) {
+			if(antPathMatcher.match(pattern, path)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+}
