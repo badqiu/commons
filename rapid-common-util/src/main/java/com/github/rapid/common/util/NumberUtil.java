ccfe21416541a7073fc7b6748327096729f01888
badqiu <badqiu@gmail.com>
2020-09-30 10:19:23 +0800

    1

--- rapid-common-util/src/main/java/com/github/rapid/common/util/NumberUtil.java
+++ rapid-common-util/src/main/java/com/github/rapid/common/util/NumberUtil.java
@@ -26,9 +26,23 @@ public class NumberUtil {
 		return v == null ? null : num.longValue();
 	}
 	
+	public static long toLong(Object v,long defaultValue) {
+		Number num = toNumber(v);
+		return v == null ? defaultValue : num.longValue();
+	}
+	
 	public static Integer toInteger(Object v) {
 		Number num = toNumber(v);
 		return v == null ? null : num.intValue();
+	}
+	
+	public static int toInteger(Object v,int defaultValue) {
+		Number num = toNumber(v);
+		return v == null ? defaultValue : num.intValue();
+	}
+	
+	public static int toInt(Object v,int defaultValue) {
+		return toInteger(v,defaultValue);
 	}
 	
 	public static Double toDouble(Object v) {
@@ -36,9 +50,19 @@ public class NumberUtil {
 		return v == null ? null : num.doubleValue();
 	}
 	
+	public static double toDouble(Object v,double defaultValue) {
+		Number num = toNumber(v);
+		return v == null ? defaultValue : num.doubleValue();
+	}
+	
 	public static Float toFloat(Object v) {
 		Number num = toNumber(v);
 		return v == null ? null : num.floatValue();
+	}
+	
+	public static float toFloat(Object v,float defaultValue) {
+		Number num = toNumber(v);
+		return v == null ? defaultValue : num.floatValue();
 	}
 	
 	public static Short toShort(Object v) {
@@ -46,11 +70,21 @@ public class NumberUtil {
 		return v == null ? null : num.shortValue();
 	}
 	
+	public static short toShort(Object v,short defaultValue) {
+		Number num = toNumber(v);
+		return v == null ? defaultValue : num.shortValue();
+	}
+	
 	public static Byte toByte(Object v) {
 		Number num = toNumber(v);
 		return v == null ? null : num.byteValue();
 	}
 
+	public static byte toByte(Object v,byte defaultValue) {
+		Number num = toNumber(v);
+		return v == null ? defaultValue : num.byteValue();
+	}
+	
 	public static Boolean toBoolean(Object v) {
 		if(v == null) return null;
 		if(v instanceof Boolean) return (Boolean)v;
@@ -62,6 +96,11 @@ public class NumberUtil {
 		
 		String str = String.valueOf(v);
 		return Boolean.parseBoolean(str);
+	}
+	
+	public static boolean toBoolean(Object v,boolean defaultValue) {
+		Boolean r = toBoolean(v);
+		return r == null ? defaultValue : r;
 	}
 	
 }
