2841c556180be7ef78eb08bc6cf769f33702db5b
badqiu <badqiu@gmail.com>
2020-12-02 11:20:16 +0800

    1

---
+++ rapid-common-util/src/main/java/com/github/rapid/common/util/Timeout.java
@@ -0,0 +1,33 @@
+package com.github.rapid.common.util;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 判断是否超时的工具类
+ * 
+ * @author badqiu
+ *
+ */
+public class Timeout {
+
+	private long lastTime = SystemTimer.currentTimeMillis();
+	
+	public void updateLastTime() {
+		lastTime = SystemTimer.currentTimeMillis();
+	}
+	
+	public boolean isTimeout(long duration,TimeUnit timeUnit) {
+		return isTimeoutByTimeMills(timeUnit.toMillis(duration));
+	}
+	
+	public boolean isTimeout(Duration duration) {
+		return isTimeoutByTimeMills(duration.toMillis());
+	}
+
+	private boolean isTimeoutByTimeMills(long durationMills) {
+		long interval = SystemTimer.currentTimeMillis() - lastTime;
+		return interval >= durationMills;
+	}
+	
+}
