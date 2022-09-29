5645fccca6515929657ec7b87f9317424ac85b59
badqiu <badqiu@qq.com>
2022-09-29 16:38:29 +0800

    1

--- rapid-common-util/src/main/java/com/github/rapid/common/xstream/CustomMarshallingStrategy.java
+++ rapid-common-util/src/main/java/com/github/rapid/common/xstream/CustomMarshallingStrategy.java
@@ -12,7 +12,20 @@ import com.thoughtworks.xstream.core.TreeUnmarshaller;
 import com.thoughtworks.xstream.io.HierarchicalStreamReader;
 import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 import com.thoughtworks.xstream.mapper.Mapper;
-
+/**
+ * 方便xstream可以引用外部的bean.  如 datasource ref=""
+ * 
+ * 
+ * 配置示例:
+ * <pre>
+ * 	CustomMarshallingStrategy marshallingStrategy = new CustomMarshallingStrategy();
+ *	marshallingStrategy.setBeans(vars);
+ *	marshallingStrategy.setApplicationContext(applicationContext);
+ *	xstream.setMarshallingStrategy(marshallingStrategy);
+ * </pre>
+ * @author badqiu
+ *
+ */
 public class CustomMarshallingStrategy extends ReferenceByIdMarshallingStrategy {
 	private ApplicationContext applicationContext;
 	private Map<Object,Object> beans = new HashMap<Object,Object>();
