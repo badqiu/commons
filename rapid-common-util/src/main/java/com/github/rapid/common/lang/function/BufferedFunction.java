531b1f8f44ceab7649eca82f35d1c9ef291ce1b7
badqiu <badqiu@qq.com>
2022-10-24 18:25:03 +0800

    1

--- rapid-common-util/src/main/java/com/github/rapid/common/lang/function/BufferedFunction.java
+++ rapid-common-util/src/main/java/com/github/rapid/common/lang/function/BufferedFunction.java
@@ -1,5 +1,72 @@
 package com.github.rapid.common.lang.function;
 
-public class BufferedFunction {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.rapid.common.util.SystemTimer;
+
+
+public class BufferedFunction <T, R> extends ProxyFunction<T, R> {
+	private static Logger logger = LoggerFactory.getLogger(BufferedFunction.class);
+	
+	
+	private int bufferSize = 1000;
+	private int bufferTimeout = 500;
+	
+	private long lastSendTime = System.currentTimeMillis();
+	private List<T> bufferList = new ArrayList<T>();
+	
+	private boolean init = false;
+	
+	public BufferedFunction() {
+		super();
+	}
+
+	public BufferedFunction(Function<T, R> proxy) {
+		super(proxy);
+	}
+	
+	@Override
+	public R apply(T t)  {
+		bufferList.add(t);
+		
+		if(bufferList.size() > bufferSize) {
+			flushBuffer();
+		}else if(bufferTimeout > 0 && isTimeout()) {
+			flushBuffer();
+		}
+		
+		return null;
+	}
+
+	private void flushBuffer() {
+		if(bufferTimeout > 0) {
+			lastSendTime = SystemTimer.currentTimeMillis();
+		}
+		
+		if(bufferList == null || bufferList.isEmpty()) {
+			return;
+		}
+		
+		List<T> tempBuf = bufferList;
+		bufferList = new ArrayList<T>(bufferSize);
+		for(T item : tempBuf) {
+			realApply(item);
+		}
+		
+	}
+
+	private boolean isTimeout() {
+		return Math.abs(lastSendTime - SystemTimer.currentTimeMillis()) > bufferTimeout;
+	}
+	
+	private R realApply(T t) {
+		return super.apply(t);
+	}
+
+	
 }
