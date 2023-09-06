c5e5faa0773ac48c4474aabca0750f023f8ab92d
badqiu <badqiu@qq.com>
2023-09-06 14:26:04 +0800

    1

--- rapid-common-util/src/main/java/com/github/rapid/common/util/SpringUtil.java
+++ rapid-common-util/src/main/java/com/github/rapid/common/util/SpringUtil.java
@@ -1,8 +1,10 @@
 package com.github.rapid.common.util;
 
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
 public class SpringUtil {
+	
 	public static void initializing(Object[] array) {
 		if(array == null) return;
 		
@@ -17,6 +19,26 @@ public class SpringUtil {
 		if(obj instanceof InitializingBean) {
 			try {
 				((InitializingBean)obj).afterPropertiesSet();
+			}catch(Exception e) {
+				throw new IllegalStateException(e);
+			}
+		}
+	}
+	
+	public static void destroy(Object[] array) {
+		if(array == null) return;
+		
+		for(Object obj : array) {
+			destroy(obj);
+		}
+	}
+	
+	public static void destroy(Object obj) {
+		if(obj == null) return;
+		
+		if(obj instanceof DisposableBean) {
+			try {
+				((DisposableBean)obj).destroy();
 			}catch(Exception e) {
 				throw new IllegalStateException(e);
 			}
