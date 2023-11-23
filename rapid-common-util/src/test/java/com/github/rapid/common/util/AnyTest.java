8ebde7e56d19f7eaa46d8a7ce1d92361e5d1713a
qiubaichao <qiubaichao@xmiles.cn>
2023-11-23 15:58:13 +0800

    1

---
+++ rapid-common-util/src/test/java/com/github/rapid/common/util/AnyTest.java
@@ -0,0 +1,13 @@
+package com.github.rapid.common.util;
+
+import org.junit.Test;
+
+public class AnyTest {
+
+	@Test
+	public void test() {
+		// 1700702889244
+		// 1700703088365
+		System.out.println(System.currentTimeMillis());
+	}
+}
