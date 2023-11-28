b45122a0326bc8965d90a97d23b7472c6ce9dca4
qiubaichao <qiubaichao@xmiles.cn>
2023-11-28 10:30:35 +0800

    1

--- rapid-common-util/src/test/java/com/github/rapid/common/util/AnyTest.java
+++ rapid-common-util/src/test/java/com/github/rapid/common/util/AnyTest.java
@@ -4,8 +4,11 @@ import org.junit.Test;
 
 public class AnyTest {
 
+	
 	@Test
 	public void test() {
+		String key = "aliyun-qiu-dockerkey-Zdf23XgfgFd";
+		System.out.println(key.length());
 		// 1700702889244
 		// 1700703088365
 		System.out.println(System.currentTimeMillis());
