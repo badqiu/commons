141ed4b0ae8a8af80957229c7dbb87491e7c3ff9
qiubaichao <qiubaichao@xmiles.cn>
2023-12-12 19:09:04 +0800

    anytest

--- rapid-common-util/src/test/java/com/github/rapid/common/util/AnyTest.java
+++ rapid-common-util/src/test/java/com/github/rapid/common/util/AnyTest.java
@@ -1,5 +1,7 @@
 package com.github.rapid.common.util;
 
+import java.sql.Timestamp;
+
 import org.junit.Test;
 
 public class AnyTest {
@@ -7,10 +9,12 @@ public class AnyTest {
 	
 	@Test
 	public void test() {
-		String key = "aliyun-qiu-dockerkey-Zdf23XgfgFd";
-		System.out.println(key.length());
+//		String key = "aliyun-qiu-dockerkey-Zdf23XgfgFd";
+//		System.out.println(key.length());
 		// 1700702889244
 		// 1700703088365
-		System.out.println(System.currentTimeMillis());
+//		System.out.println(System.currentTimeMillis());
+		System.out.println(new Timestamp(1702378829182L));
+		System.out.println(new Timestamp(1702378836571L));
 	}
 }
