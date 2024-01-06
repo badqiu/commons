c5af2e7e73d89f93d6781289b14351c1ee761963
qiubaichao <qiubaichao@xmiles.cn>
2024-01-06 18:02:44 +0800

    resource

---
+++ rapid-common-util/src/main/java/com/github/rapid/common/util/ResourceUtil.java
@@ -0,0 +1,24 @@
+package com.github.rapid.common.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+
+public class ResourceUtil {
+	
+	public static String getResourceAsText(String path) {
+		if(StringUtils.isBlank(path)) return null;
+		
+		InputStream input = ResourceUtil.class.getResourceAsStream(path);
+		try {
+			return IOUtils.toString(input,"UTF-8");
+		}catch(IOException e) {
+			throw new RuntimeException("error on read classpath path:"+path,e);
+		}finally {
+			IOUtils.closeQuietly(input);
+		}
+	}
+	
+}
