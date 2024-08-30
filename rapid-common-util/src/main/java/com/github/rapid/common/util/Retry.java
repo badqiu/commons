b049c5fcde8397c3e69560a519d775f445f2aceb
qiubaichao <qiubaichao@xmiles.cn>
2024-08-30 15:41:23 +0800

    add <R>

--- rapid-common-util/src/main/java/com/github/rapid/common/util/Retry.java
+++ rapid-common-util/src/main/java/com/github/rapid/common/util/Retry.java
@@ -65,7 +65,7 @@ public class Retry<T>{
 		return exec(cmd);
 	}
 	
-	public T exec(Callable<T> command) throws RetryException{
+	public <R> R exec(Callable<R> command) throws RetryException{
 		long start = 0;
 		if(retryTimeout > 0) {
 			start = System.currentTimeMillis();
@@ -73,7 +73,7 @@ public class Retry<T>{
 		
 		while(true) {
 			try {
-				T result = command.call();
+				R result = command.call();
 				return result;
 			} catch (Exception e) {
 				logger.warn("occer error,retry execute: useRetryTimes:"+useRetryTimes+" retryTimes:"+retryTimes+" retryInterval:"+retryInterval,e);
