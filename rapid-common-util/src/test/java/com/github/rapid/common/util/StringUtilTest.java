a6499d8141f9609d1ac2dfe6d5f2afa9f5f299b4
qiubaichao <qiubaichao@lizhi.fm>
2026-01-06 14:02:34 +0800

    1

--- rapid-common-util/src/test/java/com/github/rapid/common/util/StringUtilTest.java
+++ rapid-common-util/src/test/java/com/github/rapid/common/util/StringUtilTest.java
@@ -202,16 +202,16 @@ public class StringUtilTest {
 	@Test
 	public void testSplitByCommonSeparators_SpecialCharacters2() {
 		// 测试特殊字符
-		String input = "[\"helloworld\",\"my-name\",'my.name','my_name']";
+		String input = "[\"helloworld\",\"my-name\",'my.name','my_name',123,999.99,0x111]";
 		String[] result = StringUtil.splitByCommonSeparators(input);
 		assertNotNull(result);
-		assertEquals(4, result.length);
+//		assertEquals(6, result.length);
 		
 		for(Object item : result) {
 			System.out.println(item);
 		}
 		// 注意：-、_、@不会被分割
-		assertArrayEquals(new String[] { "helloworld", "my-name", "my.name","my_name" }, result);
+		assertArrayEquals(new String[] { "helloworld", "my-name", "my.name","my_name","123","999.99","0x111" }, result);
 	}
 
 }
