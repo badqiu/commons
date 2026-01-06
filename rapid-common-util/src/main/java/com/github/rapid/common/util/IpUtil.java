a724d569e8f2191075e673609b431ae9c2ae4322
qiubaichao <qiubaichao@lizhi.fm>
2026-01-06 14:08:09 +0800

    1

--- rapid-common-util/src/main/java/com/github/rapid/common/util/IpUtil.java
+++ rapid-common-util/src/main/java/com/github/rapid/common/util/IpUtil.java
@@ -61,6 +61,10 @@ public class IpUtil {
 
 
 	private static String getFirstIp(String ipAddress) {
+		if(StringUtils.isBlank(ipAddress)) {
+			return ipAddress;
+		}
+		
 		// 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
 		if (ipAddress != null && ipAddress.length() > 15) {
 			// = 15
