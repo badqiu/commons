4dc88b2e8f02fbe74e7fabec916556d699a2cb7d
qiubaichao <qiubaichao@lizhi.fm>
2026-01-06 14:58:50 +0800

    1

--- rapid-common-util/src/test/java/com/github/rapid/common/util/SmartFormatTest.java
+++ rapid-common-util/src/test/java/com/github/rapid/common/util/SmartFormatTest.java
@@ -8,19 +8,20 @@ public class SmartFormatTest {
 
 	@Test
 	public void test() {
-    	System.out.println(SmartFormat.format(0.0000007));
-    	System.out.println(SmartFormat.format(0.000006));
-    	System.out.println(SmartFormat.format(0.00005));
-    	System.out.println(SmartFormat.format(0.0004));
-    	System.out.println(SmartFormat.format(0.003));
-    	System.out.println(SmartFormat.format(0.02));
-    	System.out.println(SmartFormat.format(0.1));
-    	System.out.println(SmartFormat.format(1.1));
-    	System.out.println(SmartFormat.format(10.12));
-    	System.out.println(SmartFormat.format(100.123));
-    	System.out.println(SmartFormat.format(1000.1234));
+    	System.out.println(SmartFormat.format(0.0000007777));
+    	System.out.println(SmartFormat.format(0.000006666));
+    	System.out.println(SmartFormat.format(0.00005555));
+    	System.out.println(SmartFormat.format(0.0004444));
+    	System.out.println(SmartFormat.format(0.0033333));
+    	System.out.println(SmartFormat.format(0.02222));
+    	System.out.println(SmartFormat.format(0.1111));
+    	System.out.println(SmartFormat.format(1.1111));
+    	System.out.println(SmartFormat.format(10.12345));
+    	System.out.println(SmartFormat.format(100.12345));
+    	System.out.println(SmartFormat.format(999.12345));
+    	System.out.println(SmartFormat.format(1000.12345));
     	System.out.println(SmartFormat.format(10000.12345));
-    	System.out.println(SmartFormat.format(100000.92345));
+    	System.out.println(SmartFormat.format(100000.12345));
 	}
 
 }
